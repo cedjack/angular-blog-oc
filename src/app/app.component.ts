7ab2b600f12ddb7d5d64f31455bb18d196687f55
cedjack <cedric.jacquet@ac-clermont.fr>
2019-01-13 00:04:40 +0100

    exercice 1 terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'post';
+
+  posts = [
+    {
+      title: 'Premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore' +
+        ' et dolore magna aliqua. Ut enim ad minim veniam, quis',
+      created_at: new Date,
+      loveIts: 0
+    },
+    {
+      title: 'Second post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore' +
+        ' et dolore magna aliqua. Ut enim ad minim veniam, quis',
+      created_at: new Date,
+      loveIts: 0
+    },
+    {
+      title: 'Troisième post',
+      content: 'Personne n\'aime la douleur en elle-même, ne la recherche et ne la souhaite, ' +
+        'tout simplement parce qu\'il s\'agit de la douleur ...',
+      created_at: new Date,
+      loveIts: 0
+    }
+  ];
+
 }
